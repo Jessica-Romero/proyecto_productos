6faae177475cf38d1a1f096048dfa91acd58d0f0
practicasit03 <practicasit03@dosfarma.com>
2023-05-20 13:46:27 +0200

    change footer

---
+++ templates/element/footer/main.php
@@ -0,0 +1,8 @@
+<?php use Cake\Core\Configure; ?>
+
+<!-- To the right -->
+<div class="float-right d-none d-sm-inline">
+    <strong>TFG 2022/2023</strong>
+</div>
+<!-- Default to the left -->
+<strong>Catalogue Products </strong>JR
