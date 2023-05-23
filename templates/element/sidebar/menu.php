33227ae2915704b63953b14b2311b4ceef237531
practicasit03 <practicasit03@dosfarma.com>
2023-05-23 21:12:59 +0200

    add new things into sidebar

--- templates/element/sidebar/menu.php
+++ templates/element/sidebar/menu.php
@@ -21,6 +21,43 @@
     </ul>
 </li>
 
-<li class="nav-item">
-    <?= $this->Html->link('<i class="fas  nav-icon "></i> Alerts', ['controller' => 'ProductStock', 'action' => 'index'], ['escape' => false, 'class' => 'nav-link active']) ?>
+<li class="nav-item has-treeview menu-open">
+    <a href="#" class="nav-link active">
+        <i class="nav-icon fas fa-tachometer-alt"></i>
+        <p>
+            Alerts Products
+            <i class="right fas fa-angle-left"></i>
+        </p>
+    </a>
+    <ul class="nav nav-treeview">
+        <li class="nav-item">
+            <?= $this->Html->link('<i class="fas  nav-icon "></i> Alerts', ['controller' => 'Alerts', 'action' => 'index'], ['escape' => false, 'class' => 'nav-link active']) ?>
+        </li>
+        <li class="nav-item">
+            <?= $this->Html->link('<i class="fas  nav-icon "></i> Stock Alerts', ['controller' => 'StockAlerts', 'action' => 'index'], ['escape' => false, 'class' => 'nav-link active']) ?>
+        </li>
+        <li class="nav-item">
+            <?= $this->Html->link('<i class="fas  nav-icon "></i> Price Alerts', ['controller' => 'PriceAlerts', 'action' => 'index'], ['escape' => false, 'class' => 'nav-link active']) ?>
+        </li>
+    </ul>
 </li>
+
+
+<li class="nav-item has-treeview menu-open">
+    <a href="#" class="nav-link active">
+        <i class="nav-icon fas fa-tachometer-alt"></i>
+        <p>
+            New Type Alert
+            <i class="right fas fa-angle-left"></i>
+        </p>
+    </a>
+    <ul class="nav nav-treeview">
+        <li class="nav-item">
+            <?= $this->Html->link('<i class="fas  nav-icon "></i> Create Stock Alert', ['controller' => 'StockAlerts', 'action' => 'add'], ['escape' => false, 'class' => 'nav-link active']) ?>
+        </li>
+        <li class="nav-item">
+            <?= $this->Html->link('<i class="fas  nav-icon "></i> Create Price Alert', ['controller' => 'PriceAlerts', 'action' => 'add'], ['escape' => false, 'class' => 'nav-link active']) ?>
+        </li>
+    </ul>
+</li>
+
